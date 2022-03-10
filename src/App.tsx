3253bc3628605c585be394a0b25aa07adc4bcb76
UROS <nonamen212@gmail.com>
2022-03-10 16:29:23 +0100

    Basic Props finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import "./App.css";
 import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
+    const personName = {
+        first: "Bruce",
+        last: "Wayne",
+    };
+
+    const nameList = [
+        {
+            first: "Bruce",
+            last: "Wayne",
+        },
+        {
+            first: "Clark",
+            last: "Kent",
+        },
+        {
+            first: "Black",
+            last: "Canary",
+        },
+        {
+            first: "Poison",
+            last: "Ivy",
+        },
+        {
+            first: "Zatanna",
+            last: "",
+        },
+    ];
+
     return (
         <div className="App">
-            <Greet name="Joe" />
+            <Greet name="Joe" messageCount={20} isLoggedIn={false} />
+            <Person name={personName} />
+            <PersonList names={nameList} />
         </div>
     );
 }
